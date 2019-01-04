f7e3d127cbf6e150880d4e18ec3ab92b6bc88a39
stefan <stefan.labuschagne@gmail.com>
2019-01-04 17:03:40 +0200

    SP Caller

--- WebApplication4/Controllers/HomeController.cs
+++ WebApplication4/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace WebApplication4.Controllers
             return View();
         }
 
-        // CreateCaseNormal - I Typed this stuff myself!
+        // CreateCaseNormal - I Typed this stuff myself!0
         public ActionResult CreateCaseNormal()
         {
 
@@ -41,10 +41,109 @@ namespace WebApplication4.Controllers
                 var DaContext = new WebApplication4.Models.Db.DAISYEntities();
 
                 //  var Calls = new 
+
                 DaContext.CreateCaseWrite(model.Name,
                     model.Surname,
                     model.Age,
-                    model.Sex,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,);
+                    model.Sex,
+                    model.Race,
+                    model.Occupation,
+                    model.MedicalAid,
+                    model.Medication,
+                    model.Address,
+                    model.City,
+                    model.Province,
+                    model.PostalCode,
+                    model.Telephone1,
+                    model.Telephone2,
+                    model.EmailAddress,
+                    model.Suburb,
+                    model.DLLocation,
+                    model.DLType,
+                    model.DLSource,
+                    model.DLStudentNumber,
+                    model.Counsellor,
+                    model.CallDate,
+                    model.ReasonADHD,
+                    model.ReasonAlzheimersDementia,
+                    model.ReasonAnxiety,
+                    model.ReasonBipolar,
+                    model.ReasonDebtPovertyUnemployment,
+                    model.ReasonDepression,
+                    model.ReasonEatingDisoirder,
+                    model.ReasonHIVAIDS,
+                    model.ReasonInformation,
+                    model.ReasonLossGriefLoneliness,
+                    model.ReasonNone,
+                    model.ReasonOCD,
+                    model.ReasonOtherIllness,
+                    model.ReasonPhysicalEmotionalAbuse,
+                    model.ReasonPostNatalDepression,
+                    model.ReasonPTSDTrauma,
+                    model.ReasonReationshipDomesticAbuse,
+                    model.ReasonSchizophrenia,
+                    model.ReasonSelfMutilation,
+                    model.ReasonSexualAbuseRape,
+                    model.ReasonSleepingDisorder,
+                    model.ReasonSocialPhobia,
+                    model.ReasonStress,
+                    model.ReasonSubstanceAbuse,
+                    model.ReasonSuicide,
+                    model.ReferDASGFaceToFace,
+                    model.ReferGP,
+                    model.ReferGovHospital,
+                    model.ReferLiteratureWebsite,
+                    model.ReferNone,
+                    model.ReferOther,
+                    model.ReferOtherNGOHelpline,
+                    model.ReferPoliceStation,
+                    model.ReferPrivateHospital,
+                    model.ReferPsychiatrist,
+                    model.ReferPsychologist,model.ReferRapeCrisis,model.ReferShelter,model.ReferSocialWorker,model.ReferSupportGroup,model.ReferTraumaCentre,
+                    model.CallSummary,
+                    model.ReasonFrequentCaller,
+                    model.ReasonBullying,
+                    model.ReasonPregnancy,
+                    model.ReasonSexuality,
+                    model.ReferRehabilitationCentre,
+                    model.ReasonBorderlinePersonalityDisorder,
+                    model.HelpLine,
+                    model.ReasonOtherReason,
+                    model.ReasonFollowUp,
+                    model.ReferUniversityStudentWellness,
+                    model.ReferSadagHelpLine,
+                    model.ReferSelfHelp,
+                    model.ReferUber,
+                    model.DLCallTimeOfDay,
+                    model.CalledOnBehalfOf,
+                    model.ActionPoints,
+                    model.ReasonRelationshipRomanticIssues,
+                    model.ReasonRelationshipFamilyIssues,
+                    model.ReferCompanyEAP,
+                    model.ReferUniversitySupportServices,
+                    model.DistressRatingBegining,
+                    model.DistressRatingEnd
+                    );
+
+
+
+  //      public virtual int CreateCaseWrite(string name, string surname, string age, string sex, string race, string occupation, string medicalAid, string medication, string address, string city, string province, string postalCode, string telephone1, string telephone2, string emailAddress, string suburb, string dLLocation, string dLType, string dLSource, string dLStudentNumber, string counsellor, Nullable<System.DateTime> callDate, Nullable<bool> reasonADHD, Nullable<bool> reasonAlzheimersDementia, Nullable<bool> reasonAnxiety, Nullable<bool> reasonBipolar, Nullable<bool> reasonDebtPovertyUnemployment, Nullable<bool> reasonDepression, Nullable<bool> reasonEatingDisoirder, Nullable<bool> reasonHIVAIDS, Nullable<bool> reasonInformation, Nullable<bool> reasonLossGriefLoneliness, Nullable<bool> reasonNone, Nullable<bool> reasonOCD, Nullable<bool> reasonOtherIllness, Nullable<bool> reasonPhysicalEmotionalAbuse, Nullable<bool> reasonPostNatalDepression, Nullable<bool> reasonPTSDTrauma, Nullable<bool> reasonReationshipDomesticAbuse, Nullable<bool> reasonSchizophrenia, Nullable<bool> reasonSelfMutilation, Nullable<bool> reasonSexualAbuseRape, Nullable<bool> reasonSleepingDisorder, Nullable<bool> reasonSocialPhobia, Nullable<bool> reasonStress, Nullable<bool> reasonSubstanceAbuse, Nullable<bool> reasonSuicide, Nullable<bool> referDASGFaceToFace, Nullable<bool> referGP, Nullable<bool> referGovHospital, Nullable<bool> referLiteratureWebsite, Nullable<bool> referNone, Nullable<bool> referOther, Nullable<bool> referOtherNGOHelpline, Nullable<bool> 
+  //          , Nullable<bool> , Nullable<bool> referTraumaCentre, string callSummary, Nullable<bool> reasonFrequentCaller, Nullable<bool> reasonBullying, Nullable<bool> 
+  //          reasonPregnancy, Nullable<bool> reasonSexuality, Nullable<bool> 
+  //          referRehabilitationCentre, Nullable<bool> 
+  //          , string helpLine, Nullable<bool> reasonOtherReason, Nullable<bool> 
+  //          reasonFollowUp, Nullable<bool> referUniversityStudentWellness, Nullable<bool> referSadagHelpLine, Nullable<bool> 
+  //          referSelfHelp, Nullable<bool> referUber, string 
+            
+            
+  //dLCallTimeOfDay, string , string actionPoints, Nullable<bool> reasonRelationshipRomanticIssues, 
+  //          Nullable<bool> reasonRelationshipFamilyIssues, Nullable<bool> referCompanyEAP, Nullable<bool> 
+  //          referUniversitySupportServices, Nullable<int> distressRatingBegining, Nullable<int> distressRatingEnd)
+
+
+
+
+
 
                 DaContext.SaveChanges();
             
