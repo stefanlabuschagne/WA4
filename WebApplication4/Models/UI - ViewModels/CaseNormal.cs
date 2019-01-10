83a006af740517f9d2a1933bb4204465cf8a0904
stefan <stefan.labuschagne@gmail.com>
2019-01-10 15:45:46 +0200

    Cleanded up the Model

--- WebApplication4/Models/UI - ViewModels/CaseNormal.cs
+++ WebApplication4/Models/UI - ViewModels/CaseNormal.cs
@@ -47,24 +47,28 @@ namespace WebApplication4.UIModels
         // public CaseNormal { get; set; }
 
 
-        [Required, Display(Name = "First Name")]
+        [Required, Display(Name = "First Name:"), MaxLength(100)]
         public string Name { get; set; }
 
-        [Required, Display(Name = "Surname")]
+        [Required, Display(Name = "Surname:"), MaxLength(100)]
         public string Surname { get; set; }
 
-        [Display(Name="Age Group")]
+        [Display(Name = "Age Group:")]
         public string Age { get; set; }
 
-        [Required, Display(Name = "Gender")]
+        [Required, Display(Name = "Gender:")]
         public string Sex { get; set; }
 
-        [Required]
+        [Required, Display(Name = "Race:")]
         public string Race { get; set; }
 
+        [Display(Name = "Occupation::"), MaxLength(50)]
         public string Occupation { get; set; }
+
+        [Display(Name = "Medical Aid:")]
         public string MedicalAid { get; set; }
 
+        [Display(Name = "Medication:")]
         public string Medication { get; set; }
 
 
@@ -73,8 +77,10 @@ namespace WebApplication4.UIModels
 
         public string City { get; set; }
 
-
+       [Display(Name="Province")] 
         public string Province { get; set; }
+
+        [Display(Name = "Postal Code"),MaxLength(4)]
         public string PostalCode { get; set; }
 
         [Required]
@@ -89,6 +95,7 @@ namespace WebApplication4.UIModels
 
         public string DLLocation { get; set; }
 
+
         public string DLType { get; set; }
 
         public string DLSource { get; set; }
@@ -102,73 +109,173 @@ namespace WebApplication4.UIModels
         [Required]
         public Nullable<System.DateTime> CallDate { get; set; }
 
+        [Display(Name = "ADHD")]
         public bool ReasonADHD { get; set; }
 
-        [Required]
+        [Display(Name = "Alzheimers Dementia")]
         public bool  ReasonAlzheimersDementia { get; set; }
+
+        [Display(Name = "Anxiety")]
         public bool  ReasonAnxiety { get; set; }
+
+        [Display(Name = "Bipolar")]
         public bool  ReasonBipolar { get; set; }
+
+        [Display(Name = "Debt Poverty Unemployment")]
         public bool  ReasonDebtPovertyUnemployment { get; set; }
+
+        [Display(Name = "Depression")]
         public bool  ReasonDepression { get; set; }
+
+        [Display(Name = "Eating Disorder")]
         public bool  ReasonEatingDisoirder { get; set; }
+
+        [Display(Name = "HIV / AIDS")]
         public bool  ReasonHIVAIDS { get; set; }
+
+        [Display(Name = "Information")]
         public bool  ReasonInformation { get; set; }
+
+        [Display(Name = "Loss / Grief / Loneliness")]
         public bool  ReasonLossGriefLoneliness { get; set; }
+
+        [Display(Name = "None")]
         public bool  ReasonNone { get; set; }
+
+        [Display(Name = "OCD")]
         public bool  ReasonOCD { get; set; }
+
+        [Display(Name = "Other Illness")]
         public bool  ReasonOtherIllness { get; set; }
+
+        [Display(Name = "Physical Emotional Abuse")]
         public bool  ReasonPhysicalEmotionalAbuse { get; set; }
+
+        [Display(Name = "Post natal Depression")]
         public bool  ReasonPostNatalDepression { get; set; }
+
+        [Display(Name = "PTSD Trauma")]
         public bool  ReasonPTSDTrauma { get; set; }
+
+        [Display(Name = "Relationship / Domestic Abuse")]
         public bool  ReasonReationshipDomesticAbuse { get; set; }
+
+        [Display(Name = "Schizophrenia")]
         public bool  ReasonSchizophrenia { get; set; }
+
+        [Display(Name = "Self Mutilation")]
         public bool  ReasonSelfMutilation { get; set; }
+
+        [Display(Name = "Sexual Abuse / Rape")]
         public bool  ReasonSexualAbuseRape { get; set; }
+
+        [Display(Name = "Sleeping Dissorder")]
         public bool  ReasonSleepingDisorder { get; set; }
+
+        [Display(Name = "Social Phobia")]
         public bool  ReasonSocialPhobia { get; set; }
+
+        [Display(Name = "Stress")]
         public bool  ReasonStress { get; set; }
+
+        [Display(Name = "Substance Abuce")]
         public bool  ReasonSubstanceAbuse { get; set; }
+
+        [Display(Name = "Suicide")]
         public bool  ReasonSuicide { get; set; }
+
+        [Display(Name = "SADAG Face to Face")]
         public bool  ReferDASGFaceToFace { get; set; }
+
+        [Display(Name = "General Practitioner")]
         public bool  ReferGP { get; set; }
+
+        [Display(Name = "Government Hospital")]
         public bool  ReferGovHospital { get; set; }
+
+        [Display(Name = "Literature / Web site")]
         public bool  ReferLiteratureWebsite { get; set; }
+
+        [Display(Name = "None")]
         public bool  ReferNone { get; set; }
+
+        [Display(Name = "Other")]
         public bool  ReferOther { get; set; }
+
+        [Display(Name = "NGO Helpline")]
         public bool  ReferOtherNGOHelpline { get; set; }
+
+        [Display(Name = "Police Station")]
         public bool  ReferPoliceStation { get; set; }
+
+        [Display(Name = "Private Hospital")]
         public bool  ReferPrivateHospital { get; set; }
+
+        [Display(Name = "Psychiatrist")]
         public bool  ReferPsychiatrist { get; set; }
+
+        [Display(Name = "Psychologist")]
         public bool  ReferPsychologist { get; set; }
+
+        [Display(Name = "Rape Crisis")]
         public bool  ReferRapeCrisis { get; set; }
+
+        [Display(Name = "Shelter")]
         public bool  ReferShelter { get; set; }
+
+        [Display(Name = "Social Worker")]
         public bool  ReferSocialWorker { get; set; }
+
+        [Display(Name = "Support Group")]
         public bool  ReferSupportGroup { get; set; }
+
+        [Display(Name = "Trauma Centre")]
         public bool  ReferTraumaCentre { get; set; }
 
         [Required, MaxLength(2000)]
         public string CallSummary { get; set; }
 
+        [Display(Name = "Frequent Caller")]
         public bool  ReasonFrequentCaller { get; set; }
+
+        [Display(Name = "Bullying")]
         public bool  ReasonBullying { get; set; }
+
+        [Display(Name = "Pregnancy")]
         public bool  ReasonPregnancy { get; set; }
+
+        [Display(Name = "Sexuallity")]
         public bool  ReasonSexuality { get; set; }
+
+        [Display(Name = "Rehab Center")]
         public bool  ReferRehabilitationCentre { get; set; }
+
+        [Display(Name = "Borderline Personality Disorder")]
         public bool  ReasonBorderlinePersonalityDisorder { get; set; }
 
         [Required]
         public string HelpLine { get; set; }
 
-
+        [Display(Name = "Other Reason")]
         public bool  ReasonOtherReason { get; set; }
+
+        [Display(Name = "Follow-up")]
         public bool  ReasonFollowUp { get; set; }
+
+        [Display(Name = "University Student Wellness")]
         public bool  ReferUniversityStudentWellness { get; set; }
+
+        [Display(Name = "SADAG Helpline")]
         public bool  ReferSadagHelpLine { get; set; }
+
+        [Display(Name = "Self help")]
         public bool  ReferSelfHelp { get; set; }
+
+        [Display(Name = "Uber")]
         public bool  ReferUber { get; set; }
 
         // Dropdown
-        [Required]
+        [Required, Display(Name="Time:")]
         public string DLCallTimeOfDay { get; set; }
 
         // Dropdown 
@@ -176,16 +283,19 @@ namespace WebApplication4.UIModels
         public string CalledOnBehalfOf { get; set; }
 
 
-        [Required, MinLength(100), DefaultValue("1." +
-            "2." +
-            "3." +
-            "4." +
-            "5.")]
+        [Required, MinLength(100)]
         public string ActionPoints { get; set; }
 
+        [Display(Name = "Relationship - Romantic Issues")]
         public bool  ReasonRelationshipRomanticIssues { get; set; }
+
+        [Display(Name = "Relationship - Family Issues")]
         public bool  ReasonRelationshipFamilyIssues { get; set; }
+
+        [Display(Name = "Company EAP")]
         public bool  ReferCompanyEAP { get; set; }
+
+        [Display(Name = "University Support Services")]
         public bool  ReferUniversitySupportServices { get; set; }
 
         [Required, DefaultValue(3),Range(1,5)]
