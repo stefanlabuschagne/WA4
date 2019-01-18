dc92a3185d5e6d53b6e374b58467c722b7433e1a
stefan <stefan.labuschagne@gmail.com>
2019-01-18 14:51:02 +0200

    Frequency per Gender Sorted.....

--- WebApplication4/Controllers/HomeController.cs
+++ WebApplication4/Controllers/HomeController.cs
@@ -13,81 +13,140 @@ namespace WebApplication4.Controllers
             return View();
         }
 
-
-        // FrequencyReport Controller Code to diaply the Data in the Model.
-        // The is no post-controller as we do not do a post from the screen.
         public ActionResult FrequencyReport()
         {
 
-            // Do something here to accecc the data via the DAL 
-            // to query the Database View and return data.
-
+            // DATABSE COPNTEXT VARIABLE I CREATE 
             var DaContext = new WebApplication4.Models.Db.DAISYEntities();
 
-            // Create a UI VIEWMODEL and populate it withj data from the VIEW.
 
-            // new ViewModel
-            // https://www.red-gate.com/simple-talk/dotnet/asp-net/revisiting-partial-view-rendering-in-asp-net-mvc/
+            // Fetch all the row of the context and passs it to the View
+            var ReporData = DaContext.Stats_By_Race___RN.ToList();
 
+            // We need to create a collection so we can add items heere, right?
+            // IEnumerable<UIModels.FrequencyReport> ReporData;
 
-           IEnumerable frs <WebApplication4.Models.Db.DAISYEntities.FrequencyReport> fr =  DaContext.Stats_By_Race___RN.Where(x => x.Oct > 100).ToList()
+            List<UIModels.FrequencyReport> FReportItems = new List<UIModels.FrequencyReport>();        
 
-
-
-            // THIS WILL GET TO A DATALAYER COLLECTION SOMEHOW.
-
-           WebApplication4.UIModels.FrequencyReport frp = new WebApplication4.UIModels.FrequencyReport();
-
-
-            foreach (var f in frs)
-
+            //
+            foreach ( var DaMeasure in ReporData)
             {
+                // Create New Object
+                UIModels.FrequencyReport DDD = new UIModels.FrequencyReport();
 
-                frp.Jan = f.Jan;
-                frp.Feb = f.feb;
-                frp.Des = f.Dec;
-                frp.Total = f.Total;
+                // Set the Properties
+                DDD.Measure = DaMeasure.Measure.ToString();
+                DDD.Jan = DaMeasure.Jan.GetValueOrDefault();
+                DDD.Feb = DaMeasure.Feb.GetValueOrDefault();
 
-            }           
+                DDD.Mar = DaMeasure.Mar.GetValueOrDefault();
+                DDD.Apr = DaMeasure.Apr.GetValueOrDefault();
 
-
-           // https://stackoverflow.com/questions/27108846/how-to-map-data-access-to-business-logic-objects-in-entity-framework
-
-           /*
-           Yes, there is a bunch of left-right mapping to "convert" one class to another, 
-           but it's a clean separation of internal business logic classes for later on. 
-           Straight table to POCO conversions seems silly at first, but just wait until your 
-           DBA want's to normalize a few fields or you decide to add a collection to those 
-           simple objects.Being able to manage your business objects without breaking the rest of you app is priceless.
-           */
+                DDD.May = DaMeasure.May.GetValueOrDefault();
+                DDD.Jun = DaMeasure.Jun.GetValueOrDefault();
 
 
+                DDD.Jul = DaMeasure.Jul.GetValueOrDefault();
+                DDD.Aug = DaMeasure.Aug.GetValueOrDefault();
+
+                DDD.Sep = DaMeasure.Sep.GetValueOrDefault();
+                DDD.Oct = DaMeasure.Oct.GetValueOrDefault();
+
+                DDD.Nov = DaMeasure.Nov.GetValueOrDefault();
+                DDD.Des = DaMeasure.Dec.GetValueOrDefault();
+
+                DDD.Total = DaMeasure.Total.GetValueOrDefault();
+
+                // ETC;
+                // Add The object
+                FReportItems.Add(DDD);
+
+            }
 
 
-
-           // Create the UI Model with data (Work on this!)
-           WebApplication4.UIModels.FrequencyReport FC = new WebApplication4.UIModels.FrequencyReport();
-
-
-            ViewBag.Message = "Frequency Report";
-
-            return View(Helplines);
+            // Make sure teh view can acceprt an model of type IEnumerable
+            return View(FReportItems);
 
         }
 
 
 
 
+        // FrequencyReport Controller Code to diaply the Data in the Model.
+        // The is no post-controller as we do not do a post from the screen.
+    //    public ActionResult FrequencyReport_OLD()
+    //    {
 
-        // CreateCaseNormal - I Typed this stuff myself!0
-        public ActionResult CreateCaseNormal()
-        {
+    //        // Do something here to accecc the data via the DAL 
+    //        // to query the Database View and return data.
 
-            ViewBag.Message = "Create New Case";
+    //        var DaContext = new WebApplication4.Models.Db.DAISYEntities();
 
-            return View();
+    //        // Create a UI VIEWMODEL and populate it withj data from the VIEW.
+
+    //        // new ViewModel
+    //        // https://www.red-gate.com/simple-talk/dotnet/asp-net/revisiting-partial-view-rendering-in-asp-net-mvc/
+
+
+    //       IEnumerable frs <WebApplication4.Models.Db.DAISYEntities.FrequencyReport> fr =  DaContext.Stats_By_Race___RN.Where(x => x.Oct > 100).ToList()
+
+
+
+    //        // THIS WILL GET TO A DATALAYER COLLECTION SOMEHOW.
+
+    //       WebApplication4.UIModels.FrequencyReport frp = new WebApplication4.UIModels.FrequencyReport();
+
+
+
+    //        foreach (var f in frs)
+
+    //        {
+
+    //            frp.Jan = f.Jan;
+    //            frp.Feb = f.feb;
+    //            frp.Des = f.Dec;
+    //            frp.Total = f.Total;
+
+    //        }           
+
+
+    //       // https://stackoverflow.com/questions/27108846/how-to-map-data-access-to-business-logic-objects-in-entity-framework
+
+    //       /*
+    //       Yes, there is a bunch of left-right mapping to "convert" one class to another, 
+    //       but it's a clean separation of internal business logic classes for later on. 
+    //       Straight table to POCO conversions seems silly at first, but just wait until your 
+    //       DBA want's to normalize a few fields or you decide to add a collection to those 
+    //       simple objects.Being able to manage your business objects without breaking the rest of you app is priceless.
+    //       */
+
+
+
+
+
+    //       // Create the UI Model with data (Work on this!)
+    //       WebApplication4.UIModels.FrequencyReport FC = new WebApplication4.UIModels.FrequencyReport();
+
+
+    //        ViewBag.Message = "Frequency Report";
+
+    //        return View(Helplines);
+
+    //    }
+
+
+
+
+
+    //    // CreateCaseNormal - I Typed this stuff myself!0
+    //    public ActionResult CreateCaseNormal()
+    //    {
+
+    //        ViewBag.Message = "Create New Case";
+
+    //        return View();
       
-    }
+    //}
 
         // CreateCaseNormal - I Typed this stuff myself!
         // Thuis happpensd on the POST - When the form gets submitted back to the serevr.
--- WebApplication4/Models/UI - ViewModels/CaseNormal.cs
+++ WebApplication4/Models/UI - ViewModels/CaseNormal.cs
@@ -307,22 +307,37 @@ namespace WebApplication4.UIModels
         public string Name { get; set; }
     }
 
+
+    // TESTING 123 - WIP CODE
     public class FrequencyReport
     {
-
+        [Display(Name="Measure")]
         public string Measure { get; set; }
+
         public int Jan { get; set; }
+
         public int Feb { get; set; }
+
         public int Mar { get; set; }
+
         public int Apr { get; set; }
+
         public int May { get; set; }
+
         public int Jun { get; set; }
+
         public int Jul { get; set; }
-        public int Jug { get; set; }
+
+        public int Aug { get; set; }
+
         public int Sep { get; set; }
+
         public int Oct { get; set; }
+
         public int Nov { get; set; }
+
         public int Des { get; set; }
+
         public int Total { get; set; }
 
 
